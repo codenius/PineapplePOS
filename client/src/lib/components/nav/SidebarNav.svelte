835ca12f5d5acded2d8cce313db96dcc53c5d589
codenius <94353572+codenius@users.noreply.github.com>
2022-10-20 19:01:17 +0000

    feat(client): add navigation in sidebar

---
+++ client/src/lib/components/nav/SidebarNav.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { beforeNavigate } from "$app/navigation";
+  import { Nav } from "sveltestrap";
+  import NavPart from "./NavPart.svelte";
+  let expanded: boolean = false;
+
+  function toogleSidebar(): void {
+    expanded = !expanded;
+    console.log(expanded);
+  }
+
+  beforeNavigate(() => {
+    expanded = false;
+  });
+</script>
+
+<nav class="h-100 sticky-top me-3">
+  <Nav
+    pills
+    vertical
+    class="nav flex-nowrap {expanded ? 'expanded' : ''} 
+    bg-light border-end d-flex flex-column h-100"
+  >
+    <NavPart
+      on:click={toogleSidebar}
+      icon="list"
+      class="text-black bg-white"
+      bind:expanded>Menü</NavPart
+    >
+    <NavPart disabled tabindex="-1" icon="three-dots" class="opacity-0" />
+    <NavPart href="/" icon="house-fill" bind:expanded>Start</NavPart>
+    <NavPart href="/shop" icon="shop" bind:expanded>Shop</NavPart>
+    <NavPart href="/stock" icon="boxes" class="border-bottom-0" bind:expanded
+      >Lager</NavPart
+    >
+  </Nav>
+</nav>
+
+<style global>
+  nav .nav {
+    transition: 0.2s ease max-width;
+    max-width: 4rem;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+  nav .expanded {
+    max-width: 10rem !important;
+  }
+
+  /* nav .nav-link span {
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 200ms ease-out, visibility 0ms 200ms;
+  }
+  nav .expanded span {
+    opacity: 1;
+    visibility: visible;
+    transition: opacity 200ms ease-out 100ms;
+  } */
+</style>
---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { Styles } from "sveltestrap"; // component, that includes Bootstrap
+  import SidebarNav from "$lib/components/nav/SidebarNav.svelte";
+  import { beforeNavigate, afterNavigate } from "$app/navigation";
+  import ProgressBar from "svelte-progress-bar";
+
+  let progress: any;
+  beforeNavigate(() => {
+    progress.start();
+  });
+  afterNavigate(() => {
+    progress.complete();
+  });
+</script>
+
+<!-- includes Bootstrap stylesheet -->
+<Styles />
+
+<ProgressBar bind:this={progress} />
+
+<div id="wrapper">
+  <SidebarNav />
+  <main><slot /></main>
+</div>
+
+<style>
+  :global(html, body) {
+    height: 100%;
+    overflow-y: hidden;
+  }
+  #wrapper {
+    height: 100%;
+    display: flex;
+    overflow-y: scroll;
+  }
+  :global(.svelte-progress-bar, .svelte-progress-bar-leader) {
+    background-color: var(--bs-primary);
+    z-index: 1030; /* this is bootstraps z-index for position fixed things */
+  }
+</style>
