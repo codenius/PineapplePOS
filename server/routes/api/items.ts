478ad91cccca98205a745b71a80dd60ec71420ec
thefentos <98616003+thefentos@users.noreply.github.com>
2022-10-27 12:31:05 +0200

    Create some ItemEndpoints

--- server/routes/api/items.ts
+++ server/routes/api/items.ts
@@ -1,9 +1,28 @@
 import ItemController from "../../controllers/api/itemController";
 import {Router} from "express";
 
-const itemRouter = Router()
+const itemRouter = Router();
 
 /* Item Router paths using the ItemController */
 
+itemRouter.get('',(req,res)=>{
+    /**
+     * * Docs: https://github.com/DoctorFuchs/POS/blob/master/docs/api.md#get-apiitems
+     */
+
+    res.json(ItemController.get(req,res,(e)=>{res.json({error: e})}))
+    
+})
+
+itemRouter.post('new',(req,res)=>{
+    /**
+     * * Docs: https://github.com/DoctorFuchs/POS/blob/master/docs/api.md#post-apiitemnew
+     */
+
+    const i = ItemController.create(req,res,(e)=>{res.json({error: e})})
+
+    res.json(i)
+
+})
 
 export default itemRouter
