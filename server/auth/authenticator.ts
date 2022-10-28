1a25a7f2484e5d9c7f4306fd07eab0f6906ec75f
thefentos <98616003+thefentos@users.noreply.github.com>
2022-10-28 10:52:38 +0200

    fix: local bug fix

---
+++ server/auth/authenticator.ts
@@ -0,0 +1,19 @@
+class Authenticator {
+    static admin(req, res, next) {
+        next()
+    }
+
+    static edit(req, res, next) {
+        next()
+    }
+
+    static sell(req, res, next) {
+        next()
+    }
+
+    static read(req, res, next) {
+        next()
+    }
+}
+
+export default Authenticator
