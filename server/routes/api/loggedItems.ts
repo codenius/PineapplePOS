f4cc78e060cb915963ab70b6895f6767dea22493
thefentos <98616003+thefentos@users.noreply.github.com>
2022-10-29 16:43:45 +0200

    feat: finished loggedItemRouter

--- server/routes/api/loggedItems.ts
+++ server/routes/api/loggedItems.ts
@@ -1,9 +1,18 @@
 import loggedItemController from "../../controllers/api/loggedItemController";
 import {Router} from "express";
+import Authenticator from "../../auth/authenticator";
 
 const loggedItemRouter = Router()
 
 /* Logged Item Router paths using the LoggedItemController */
+
+/** 
+ * * Docs: https://github.com/DoctorFuchs/POS/blob/master/docs/api.md#logged-items
+ */
+
+loggedItemRouter.get('', [Authenticator.read, loggedItemController.get])
+loggedItemRouter.get(':id:', [Authenticator.read, loggedItemController.get])
+
 
 
 export default loggedItemRouter
