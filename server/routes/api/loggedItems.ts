cde106ecc491e3c95a5c1e76161438271fb8be3c
thefentos <98616003+thefentos@users.noreply.github.com>
2022-10-29 17:43:26 +0200

    fix(server): Windows Bug by Exec fixed

--- server/routes/api/loggedItems.ts
+++ server/routes/api/loggedItems.ts
@@ -10,6 +10,16 @@ const loggedItemRouter = Router()
  * * Docs: https://github.com/DoctorFuchs/POS/blob/master/docs/api.md#logged-items
  */
 
+loggedItemRouter.get('/', [
+    (req,res,next)=>Authenticator.read(req,res,next),
+    (req,res)=>LoggedItemController.get(req,res)
+])
+loggedItemRouter.get('/:id:', [
+    (req,res,next)=>Authenticator.read(req,res,next),
+    (req,res)=>LoggedItemController.getSingle(req,res)
+])
+
+
 loggedItemRouter.get('', [Authenticator.read, loggedItemController.get])
 loggedItemRouter.get(':id:', [Authenticator.read, loggedItemController.get])
 
