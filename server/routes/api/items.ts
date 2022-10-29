627efb98f4052e0073489e0155ba1e26979be33e
thefentos <98616003+thefentos@users.noreply.github.com>
2022-10-29 16:30:06 +0200

    feat: Finished itemRouter

--- server/routes/api/items.ts
+++ server/routes/api/items.ts
@@ -1,4 +1,5 @@
 import ItemController from "../../controllers/api/itemController";
+import ActionController from "../../controllers/api/actionController";
 import {Router} from "express";
 import Authenticator from "../../auth/authenticator";
 
@@ -6,18 +7,18 @@ const itemRouter = Router();
 
 /* Item Router paths using the ItemController */
 
-/*
- * Docs: https://github.com/DoctorFuchs/POS/blob/master/docs/api.md#
+/* 
+ * Docs: https://github.com/DoctorFuchs/POS/blob/master/docs/api.md#items
  */
+itemRouter.get('', [Authenticator.read, ItemController.get])
+itemRouter.post('/new', [Authenticator.edit, ItemController.create])
+itemRouter.get(':id:', [Authenticator.read, ItemController.getSingle])
+itemRouter.delete(':id:', [Authenticator.edit, ItemController.deleteSingle])
+itemRouter.put(':id:', [Authenticator.edit, ItemController.editSingle])
+itemRouter.get(':id:/recover', [Authenticator.read, ItemController.recoverSingle])
+itemRouter.get(':id:/actions', [Authenticator.read, ActionController.get])
+itemRouter.delete(':id:/actions', [Authenticator.edit, ActionController.delete])
 
-itemRouter.get('/', [
-    (req, res, next) => Authenticator.read(req, res, next),
-    (req, res) => ItemController.get(req, res)
-])
 
-itemRouter.post('/new', [
-    (req, res, next) => Authenticator.edit(req, res, next),
-    (req, res) => ItemController.create(req, res)
-])
 
 export default itemRouter
