db0423bedf54c98da25f2539fd5fb93f4a2a5d88
DoctorFuchs <fuchs9203@gmail.com>
2022-11-06 19:32:38 +0100

    docs(server): wrote comments for errors and validator

--- server/types/errors/databaseError.ts
+++ server/types/errors/databaseError.ts
@@ -1,6 +1,17 @@
 import BaseError from "./baseError";
 
+/**
+ * ## Database Error
+ *
+ * Describes all errors that depends on database.
+ */
 class DatabaseError extends BaseError {
+    /**
+     * Inits a database error
+     *
+     * @param msg - a custom msg
+     * @default msg - gives feedback, that this is an error that shouldn't happen
+     */
     constructor(msg: string = "If you see this error message, please report it") {
         super(msg, "database");
         this.name = "DatabaseError"
--- server/types/errors/methodError.ts
+++ server/types/errors/methodError.ts
@@ -1,7 +1,20 @@
 import BaseError from "./baseError";
 import {ErrorSource} from "./commons";
 
+/**
+ * ## Method error
+ *
+ * Error to show that a http method isn't allowed
+ */
 class MethodError extends BaseError {
+    /**
+     * Inits a method error
+     *
+     * @param msg - a custom error message,
+     * @default msg - Method is not allowed - should be fine (in common)
+     * @param src - a Error Source.
+     * @default src - internal, the user doesn't choose the request method, so it's our fault
+     */
     constructor(msg= "Method is not allowed", src: ErrorSource = "internal") {
         super(msg, src);
         this.name = "MethodError"
