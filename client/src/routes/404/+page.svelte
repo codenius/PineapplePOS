9e812a5984bff1758512e4fad9346258ea5ab6aa
codenius <94353572+codenius@users.noreply.github.com>
2022-11-18 13:37:09 +0000

    build(client): add nginx not found error page

---
+++ client/src/routes/404/+page.svelte
@@ -0,0 +1,8 @@
+<div class="d-flex justify-content-center align-items-center h-100">
+	<div class="d-flex flex-column align-items-center">
+		<h1 class="m-auto">404: Not Found</h1>
+		<h6 class="text-muted">
+			The requested page does not exist. <a href="/">Return to Start</a>
+		</h6>
+	</div>
+</div>
