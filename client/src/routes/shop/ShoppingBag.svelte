3e2248d553f8bced6decbd1d7d6385ae6d0cd12a
codenius <94353572+codenius@users.noreply.github.com>
2022-11-18 14:27:57 +0000

    feat(client): animate shopping bag

--- client/src/routes/shop/ShoppingBag.svelte
+++ client/src/routes/shop/ShoppingBag.svelte
@@ -2,19 +2,27 @@
 	import { shoppingBag } from '$lib/stores/shoppingBag';
 	import { useQuery } from '@sveltestack/svelte-query';
 	import { ListGroup } from 'sveltestrap';
+	import { flip } from 'svelte/animate';
+	import { fly } from 'svelte/transition';
 	import ShoppingBagEntry from './components/ShoppingBag/ShoppingBagEntry.svelte';
 	import type { Item } from '$lib/types/Item';
 
 	let queryResult = useQuery<Item[], Error>('items');
 </script>
 
-<ListGroup class="p-2">
+<ListGroup class="p-2" style="overflow-x: hidden">
 	{#if $queryResult.isSuccess}
-		{#each $shoppingBag as { id, amount }}
-			<ShoppingBagEntry
-				{...$queryResult.data.find((item) => item.id == id)}
-				shoppingBagAmount={amount}
-			/>
+		{#each $shoppingBag as { id, amount } (id)}
+			<span
+				animate:flip={{ duration: 200 }}
+				in:fly={{ y: -10, duration: 300 }}
+				out:fly={{ y: 5, duration: 100 }}
+			>
+				<ShoppingBagEntry
+					{...$queryResult.data.find((item) => item.id == id)}
+					shoppingBagAmount={amount}
+				/>
+			</span>
 		{/each}
 		<div style="height: 5rem;" />
 	{/if}
