e3d2f8d1a291a471625b14400d11dd8d90d99b77
codenius <94353572+codenius@users.noreply.github.com>
2022-11-19 16:27:59 +0100

    style(client): format shoppingBag.ts

--- client/src/lib/stores/shoppingBag.ts
+++ client/src/lib/stores/shoppingBag.ts
@@ -4,4 +4,4 @@ import { writable } from 'svelte/store';
 const bag: ShoppingBagEntry[] = [];
 export const shoppingBag = writable(bag);
 
-export const clearedShoppingBag = writable(bag)
+export const clearedShoppingBag = writable(bag);
