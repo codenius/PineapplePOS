69d174be8ac68af7b354989851da1c71f9e12762
DoctorFuchs <fuchs9203@gmail.com>
2022-11-26 10:06:09 +0100

    feat(server): added actions routes to items

--- server/routes/api/items.ts
+++ server/routes/api/items.ts
@@ -3,6 +3,8 @@ import {Router} from "express";
 import Authenticator from "../../auth/authenticator";
 import ActionController from "../../controllers/api/actionController";
 import SimpleController from "../../controllers/simpleController";
+import validate from "../../types/validator";
+import {Types} from "mongoose";
 
 const itemRouter = Router();
 
@@ -24,6 +26,19 @@ itemRouter.get('/', [
 itemRouter.post('/new', [
     (req,res,next) => Authenticator.edit(req,res,next),
     (req,res) => ItemController.create.single(req,res)
+])
+
+/**
+ * Get all item categories
+ *
+ * @access - Level:Read
+ */
+itemRouter.get("/categories", [
+    (req, res, next) => Authenticator.read(req, res, next),
+    async (req, res) => {
+        let items = await ItemController.model.find().exec()
+        res.status(200).json([...new Set(items.map(item => item["category"]))])
+    }
 ])
 
 /**
@@ -56,29 +71,22 @@ itemRouter.put('/:id', [
     (req,res) => ItemController.update.single(req,res)
 ])
 
-/* TODO: NEED AN UPDATE OF THE SIMPLE CONTROLLER (for DoctorFuchs)
-itemRouter.get('/:id:/recover', [
-    (req,res,next) => Authenticator.edit(req,res,next),
-    (req,res) => ItemController.recoverSingle(req,res)
+
+itemRouter.get("/:id/actions", [
+    (req, res, next) => Authenticator.read(req, res, next),
+    (req, res) => {
+        validate(Types.ObjectId, req.params.id)
+        req.body.orginal_item_id = req.params.id
+        ActionController.select.all(req, res)
+    }
 ])
 
-itemRouter.get('/:id:/actions', [
-    (req,res,next) => Authenticator.read(req,res,next),
-    (req,res) => ActionController.get(req,res, {
-        deleted: false,
-        old_item: {
-            original_item_id: SimpleController.validateId(req.params.id) // validate ID to refuse any injection
-        }})
+itemRouter.get("/:item_id/actions/:id", [
+    (req, res, next) => Authenticator.read(req, res, next),
+    (req, res) => {
+        validate(Types.ObjectId, req.params.id)
+        req.body.orginal_item_id = req.params.id
+        ActionController.select.single(req, res)
+    }
 ])
-
-itemRouter.delete('/:id:/actions', [
-    (req,res,next) => Authenticator.edit(req,res,next),
-    (req,res) => ActionController.delete(req,res, {
-        deleted: false,
-        old_item: {
-            original_item_id: SimpleController.validateId(req.params.id) // validate ID to refuse any injection
-        }})
-])
-*/
-
 export default itemRouter
