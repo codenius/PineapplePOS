e4755677689c9f2cadaa87b0271b460b035ee27a
codenius <94353572+codenius@users.noreply.github.com>
2022-12-23 18:37:48 +0000

    refactor(client): modular shopping bag entry image

---
+++ client/src/lib/components/ListItemImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { Item } from '$lib/types/Item';
+	export let image: Item['image'];
+</script>
+
+<span class="rounded border p-1" style="height: 3rem; aspect-ratio: 1 / 1;">
+	<img class="h-100 w-100" style="object-fit: contain;" src={image} alt="" />
+</span>
