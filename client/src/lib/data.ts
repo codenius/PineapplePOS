b306dc6cc33ab35932069fefaeeec84cd29c7dc9
codenius <94353572+codenius@users.noreply.github.com>
2023-02-13 16:31:06 +0000

    feat(client): add sell logic

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -1,6 +1,7 @@
 import { writable, type Writable } from 'svelte/store';
 import data from './example.json';
 import type { Item } from './types/Item';
+import type { ShoppingBagEntry } from './types/ShoppingBagEntry';
 
 const itemsStore: Writable<Item[]> = writable(data as unknown as Item[]);
 
@@ -32,4 +33,20 @@ export function deleteItem(id: Item['id']) {
 	const newDb = database.slice();
 	newDb.splice(index, 1);
 	itemsStore.set(newDb);
+}
+
+function sellItem(shoppingBagEntry: ShoppingBagEntry) {
+	const index = database.findIndex(
+		(dbItem) => dbItem.id == shoppingBagEntry.id
+	);
+	const newDb = database.slice();
+	if (newDb[index].amount >= shoppingBagEntry.amount) {
+		newDb[index].amount -= shoppingBagEntry.amount;
+	}
+}
+
+export function sellItems(shoppingBag: ShoppingBagEntry[]) {
+	shoppingBag.forEach((shoppingBagEntry) => {
+		sellItem(shoppingBagEntry);
+	});
 }
