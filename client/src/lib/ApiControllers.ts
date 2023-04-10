46b105737b418c4f296c65e51ea7fd0fac497dd7
codenius <94353572+codenius@users.noreply.github.com>
2023-04-10 10:47:56 +0000

    fix(client): load api controllers synchronously

--- client/src/lib/ApiControllers.ts
+++ client/src/lib/ApiControllers.ts
@@ -1,32 +1,24 @@
 import { env } from '$env/dynamic/public';
 
 import type { Item } from '$lib/types/Item';
-import type { ItemsController as ApiItemsController } from './ApiControllers/items/ItemsController';
-import type { ItemsController as MockItemsController } from './ApiControllers/items/LocalStorageItemsController';
+import items from './ApiControllers/items/example.json';
+import { ItemsController as ApiItemsController } from './ApiControllers/items/ItemsController';
+import { ItemsController as MockItemsController } from './ApiControllers/items/LocalStorageItemsController';
 
 import type { Employee } from '$lib/types/Employee';
-import type { EmployeesController as MockEmployeesController } from './ApiControllers/employees/LocalStorageEmployeesController';
+import employees from './ApiControllers/employees/example.json';
+import { EmployeesController as MockEmployeesController } from './ApiControllers/employees/LocalStorageEmployeesController';
 
 export let ItemsController: ApiItemsController | MockItemsController;
 export let EmployeesController: MockEmployeesController;
 
 (async () => {
 	if (env.PUBLIC_MOCK_API == 'true') {
-		ItemsController = new (
-			await import('./ApiControllers/items/LocalStorageItemsController')
-		).ItemsController(
-			(await import('./ApiControllers/items/example.json'))
-				.default as unknown as Item[]
-		);
-		EmployeesController = new (
-			await import('./ApiControllers/employees/LocalStorageEmployeesController')
-		).EmployeesController(
-			(await import('./ApiControllers/employees/example.json'))
-				.default as unknown as Employee[]
+		ItemsController = new MockItemsController(items as unknown as Item[]);
+		EmployeesController = new MockEmployeesController(
+			employees as unknown as Employee[]
 		);
 	} else {
-		ItemsController = new (
-			await import('./ApiControllers/items/ItemsController')
-		).ItemsController(env.PUBLIC_BACKEND_URL || '/api');
+		ItemsController = new ApiItemsController(env.PUBLIC_BACKEND_URL || '/api');
 	}
 })();
